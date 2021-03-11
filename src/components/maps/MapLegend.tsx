0eb181d07bb08eb500ffad3f3c2268d32a8e9d35
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-03-11 11:18:16 -0500

    Create map legend component

---
+++ src/components/maps/MapLegend.tsx
@@ -0,0 +1,79 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+
+import React, { useState } from 'react';
+import { css, jsx } from '@emotion/react';
+
+export interface MapLegendProps {
+  colorScale: Array<string>,
+  legendEntries: Array<string>,
+  title: string,
+}
+
+const legendWrapperStyle = css`
+  background-color: rgba(255,255,255,0.8);
+  max-width: 168px;
+  min-height: 20px;
+  padding: 5px 15px;
+  position: absolute;
+  right: 8px;
+  top: 8px;
+  width: 168px;
+  z-index: 10;
+`;
+
+const legendListStyle = css`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+`;
+
+const legendEntryStyle = css`
+  align-items: flex-start;
+  color: #1f4e46;
+  font-family: 'Montserrat', sans-serif;
+  font-size: .75rem;
+  font-weight: 300;
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 10px;
+`;
+
+const legendTitleStyle = css`
+  color: #1f4e46;
+  font-family: 'Montserrat', sans-serif;
+  font-size: .75rem;
+  font-weight: 700;
+  margin-bottom: 16px;
+`;
+
+function setLegend(legendEntries: Array<string>, colorScale: Array<string>) {
+  return legendEntries.map((entry: string, i: number) => (
+    <li key={entry} css={legendEntryStyle}>
+      <svg width="21" height="20" css={css`margin-right: 4px;`}>
+        <rect x="2" y="2" width="16" height="16" fill={colorScale[i]} stroke="black" />
+      </svg>
+      <span className="map-legend__entry">{entry}</span>
+    </li>
+  ));
+}
+
+export const MapLegend: React.FC<MapLegendProps> = ({ legendEntries, colorScale, title, children }) => {
+  const [isExpanded, setExpansion] = useState(true);
+  return (
+    <div css={legendWrapperStyle}>
+      { isExpanded ? (
+        <React.Fragment>
+          <span css={legendTitleStyle}>{title}</span>
+          <ul css={legendListStyle}>
+            {setLegend(legendEntries, colorScale)}
+          </ul>
+          {children}
+        </React.Fragment>
+      ) : <span css={legendTitleStyle}>Expand Legend</span>}
+      <button type="button" css={css`float: right; margin-left: 16px;`} onClick={() => setExpansion(!isExpanded)}>
+        {isExpanded ? '-' : '+'}
+      </button>
+    </div>
+  );
+};
--- src/stories/Map.stories.tsx
+++ src/stories/Map.stories.tsx
@@ -3,12 +3,15 @@ import { Story, Meta } from '@storybook/react';
 import { Map, MapProps } from '../components/maps/Map';
 import { NavigationControl } from '../components/maps/NavigationControl';
 import { MunicipalitiesMAPC } from '../components/maps/MunicipalitiesMAPC';
+import { MapLegend } from '../components/maps/MapLegend';
 import { data } from '../assets/WasteWaterData';
 
 export default {
   title: 'Maps/Basemap',
   component: Map
 } as Meta;
+
+const sewerColors = ['red', 'goldenrod', 'blue']
 
 const Template: Story<MapProps> = (args) => <Map {...args} />
 const NavTemplate: Story<MapProps> = (args) => (
@@ -25,7 +28,7 @@ const MAPCTemplate: Story<MapProps> = (args) => (
       data={data}
       matchOn="municipal"
       mapColumn="sewer_flag"
-      colorScale={['red', 'goldenrod', 'blue']}
+      colorScale={sewerColors}
       choroplethFunc={(colorScale: Array<string>, value: number|string) => {
         if (+value === 0) {
           return colorScale[0]
@@ -34,6 +37,11 @@ const MAPCTemplate: Story<MapProps> = (args) => (
         }
         return colorScale[2]
       }}
+    />
+    <MapLegend
+      colorScale={sewerColors}
+      title="Sewer Systems"
+      legendEntries={['No centralized sewer system(s)', 'Fully sewered, or nearly so', 'Partially sewered']}
     />
   </Map>
 )
@@ -45,4 +53,4 @@ export const MapWithNavigation = NavTemplate.bind({});
 MapWithNavigation.args = {container: 'map', accessToken: 'pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg'};
 
 export const MAPCLayer = MAPCTemplate.bind({});
-MAPCLayer.args = {container: 'map', accessToken: 'pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg', style: 'mapbox://styles/ihill/ckcnnn63u26o11ip2qf4odwyp'};
+MAPCLayer.args = {container: 'map', accessToken: 'pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg'};
