5a01109d826fd989f026678bb89fb6c7b2397824
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-03-12 11:19:37 -0500

    Fix map component import

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,14 @@ import { SocialIcon } from './components/SocialIcon';
 import { Accordion } from './components/Accordion';
 import { Logo } from './components/Logo';
 import { MetrocommonTriangle } from './components/MetrocommonTriangle';
-import * as Maps from './components/maps/index';
+
+import { Map } from './components/maps/Map';
+import { Source } from './components/maps/Source';
+import { Layer } from './components/maps/Layer';
+import { NavigationControl } from './components/maps/NavigationControl';
+import { MunicipalitiesMAPC } from './components/maps/MunicipalitiesMAPC';
+import { Tooltip } from './components/maps/Tooltip';
+import { MapLegend } from './components/maps/MapLegend';
 
 export {
   Footer,
@@ -13,5 +20,11 @@ export {
   Accordion,
   Logo,
   MetrocommonTriangle,
-  Maps
+  Map,
+  Source,
+  Layer,
+  NavigationControl,
+  MunicipalitiesMAPC,
+  Tooltip,
+  MapLegend
 };
