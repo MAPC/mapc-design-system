7d5400154c5d2c1d29eae437beb496ca94e03967
NotLiu <aliu@lediur.com>
2023-09-25 17:05:08 -0400

    add user dropdown

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
-import { Footer } from './components/Footer';
-import { SocialNav } from './components/SocialNav';
-import { SocialIcon } from './components/SocialIcon';
-import { Accordion } from './components/Accordion';
-import { Logo } from './components/Logo';
-import { MetrocommonTriangle } from './components/MetrocommonTriangle';
+import { Footer } from "./components/Footer";
+import { SocialNav } from "./components/SocialNav";
+import { SocialIcon } from "./components/SocialIcon";
+import { Accordion } from "./components/Accordion";
+import { Logo } from "./components/Logo";
+import { MetrocommonTriangle } from "./components/MetrocommonTriangle";
 
-import { Map } from './components/maps/Map';
-import { Source } from './components/maps/Source';
-import { Layer } from './components/maps/Layer';
-import { NavigationControl } from './components/maps/NavigationControl';
-import { MunicipalitiesMAPC } from './components/maps/MunicipalitiesMAPC';
-import { Tooltip } from './components/maps/Tooltip';
-import { MapLegend } from './components/maps/MapLegend';
+import { Map } from "./components/maps/Map";
+import { Source } from "./components/maps/Source";
+import { Layer } from "./components/maps/Layer";
+import { NavigationControl } from "./components/maps/NavigationControl";
+import { MunicipalitiesMAPC } from "./components/maps/MunicipalitiesMAPC";
+import { Tooltip } from "./components/maps/Tooltip";
+import { MapLegend } from "./components/maps/MapLegend";
+import { UserDropdown } from "./components/UserDropdown";
 
 export {
   Footer,
@@ -26,5 +27,6 @@ export {
   NavigationControl,
   MunicipalitiesMAPC,
   Tooltip,
-  MapLegend
+  MapLegend,
+  UserDropdown,
 };
