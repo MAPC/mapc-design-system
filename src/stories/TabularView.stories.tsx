229fe1c332bacae74c2e56fe75a299a81ea5f3e0
NotLiu <aliu@lediur.com>
2023-09-27 16:22:37 -0400

    add tabular view changes, themes setup

---
+++ src/stories/TabularView.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { TabularView } from "../components/TabularView";
+
+const meta = {
+  title: "components/Tabular View",
+  component: TabularView,
+} satisfies Meta<typeof TabularView>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const testData = [
+  { title: "The First Book", body: "Large desciription of text", signature: "test name" },
+  { title: "The Second Book", body: "Large desciription of text", signature: "tester" },
+  { title: "The Last Book", body: "Large desciription of text", signature: "final book name" },
+];
+
+const testDataCost = [
+  { title: "The First Book", price: 400, signature: "test name", price2: 400, signature2: "test name" },
+  { title: "The First Book", price: 200, signature: "test name", price2: 400, signature2: "test name" },
+  { title: "The First Book", price: 450, signature: "test name", price2: 400, signature2: "test name" },
+  { title: "The First Book", price: 110, signature: "test name", price2: 400, signature2: "test name" },
+  { title: "The First Book", price: 220, signature: "test name", price2: 400, signature2: "test name" },
+  { title: "The First Book", price: 250, signature: "test name", price2: 400, signature2: "test name" },
+  { title: "The First Book", price: 400, signature: "test name", price2: 400, signature2: "test name" },
+  { title: "The First Book", price: 420, signature: "test name", price2: 400, signature2: "test name" },
+  { title: "The First Book", price: 3300, signature: "test name", price2: 400, signature2: "test name" },
+];
+
+export const ViewText: Story = {
+  args: { fields: ["title", "body", "signature"], data: testData, searchTerm: "" },
+};
+
+export const ViewCost: Story = {
+  args: { fields: ["title", "price", "signature", "price2", "signature2"], data: testDataCost, searchTerm: "" },
+  decorators: [
+    (Story) => (
+      <div style={{ height: "15rem" }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
